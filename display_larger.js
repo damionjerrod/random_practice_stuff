67f9de1ab2e107d369c8aefad9da64dfdf77678b
damionjerrod <damion.dawson@gmail.com>
2019-12-17 12:29:52 -0500

    compares two numbers and displays the largest

---
+++ display_larger.js
@@ -0,0 +1,17 @@
+function displayLarger(int1, int2){
+    if(typeof(int1) !== typeof(int2) || typeof(int2) !== typeof(int1)){
+        console.log("Can't compare.");
+    }
+    else if(int1 === int2){
+        console.log("Numbers are equal.");
+    } else {
+        if(int1 > int2){
+            console.log(`${int1} is larger than ${int2}.`);
+        }
+        else if(int2 > int1){
+            console.log(`${int2} is larger than ${int1}.`);
+        }
+    }
+}
+
+displayLarger(5, 4);
